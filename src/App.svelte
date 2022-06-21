470ada6f40e35d37413f6ee4e58ca8bcd3d92e87
anirudhsnayak <anirudhsnayak@gmail.com>
2022-06-21 13:41:25 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+import PrivacyPolicy from "./app/PrivacyPolicy.svelte";
+</script>
+
+<div class="privacy-policy-container">
+	<PrivacyPolicy></PrivacyPolicy>
+	<br>
+</div>
+
+
+<style type="text/scss">
+	@import "./styles/pageSetup.scss";
+	.privacy-policy-container{
+		margin-left: 1rem;
+		margin-top: 1rem;
+	}
+</style>
+
